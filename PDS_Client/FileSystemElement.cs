7d8cc4ba4d1e95c02f957aa395c7c19989c4d0a4
Marco Montani <marco.montani.92@gmail.com>
2015-09-20 22:50:07 +0200

    initial project

---
+++ PDS_Client/FileSystemElement.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+using System.Windows.Controls;
+
+namespace PDS_Client
+{
+    public class FileSystemElement 
+    {
+        private Bitmap icon;
+        String pname; // private name
+
+        public Bitmap image {
+            get {
+                return icon;
+            }
+            set
+            {
+                icon = value;
+            }
+        }
+
+        public String name
+        {
+            get
+            {
+                return pname;
+            }
+            set
+            {
+                pname = value;
+            }
+        }
+        
+        
+
+        public FileSystemElement(Bitmap image, String name)
+        {
+            this.image = image;
+            this.pname = name;
+        }
+
+
+        
+    }
+}
---
+++ PDS_Client/LoginPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using System.Net.Sockets;
+using System.Net;
+
+
+namespace PDS_Client
+{
+    /// <summary>
+    /// Logica di interazione per Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+
+        Socket s = null;
+
+        public Window1()
+        {
+            InitializeComponent();
+            
+        }
+
+
+        public void createSocket()
+        {
+            try
+            {
+                s = new Socket(SocketType.Dgram, ProtocolType.Tcp);
+                IPAddress sAddr = new IPAddress(2130706433); //  127.0.0.1 --> 2130706433
+                if (!s.Connected) throw new SocketException();
+            }
+            catch(SocketException se)
+            {
+                // todo: send a popup ( "impossibile connettersi al server " )
+            }
+            
+        }
+    }
+}
