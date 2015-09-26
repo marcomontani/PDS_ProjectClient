718d552d3b12049b968dcf910eb68f9674fb009f
GaetanoMondelli <gaetano.mondelli@gmail.com>
2015-09-26 19:02:20 +0200

    carouselimplemented

--- PDS_Client/Registration.xaml.cs
+++ PDS_Client/Registration.xaml.cs
@@ -1,20 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 using System.Net.Sockets;
 using System.Net;
-using System.Runtime.InteropServices;
+using System.Windows.Shapes;
+using System.Windows.Media;
 
 namespace PDS_Client
 {
@@ -25,10 +17,16 @@ namespace PDS_Client
     {
 
         Socket s = null;
+        int status;
+        string username, password,path;
 
         public Registration()
         {
             InitializeComponent();
+            status = 0;
+            username = null;
+            password = null;
+            path = null;
 
         }
 
@@ -52,21 +50,7 @@ namespace PDS_Client
             
         }
 
-        private void btn_login_Click(object sender, RoutedEventArgs e)
-        {
-
-            //if (s == null) createSocket(); // the socket is already connected
-            string username = ((TextBox)this.FindName("text_user")).Text;
-            string password = ((PasswordBox)this.FindName("text_pass")).Password;
-
-            string message = "LOGIN " + username + " " + password;
-           // s.Send(Encoding.ASCII.GetBytes("ciao"));
-
-            // todo: wait for answer. if ok proceed
-
-            (new MainWindow()).Show();
-            this.Close();
-        }
+     
 
         private void mouse_MouseDown(object sender, MouseButtonEventArgs e)
         {
@@ -76,6 +60,88 @@ namespace PDS_Client
         private void btn_x_Click_1(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+
+        private void txt_wizard_GotFocus(object sender, RoutedEventArgs e)
+        {
+            ((TextBox)sender).Text = "";
+
+        }
+
+        private void elps_wizards_MouseOver(object sender, RoutedEventArgs e)
+        {
+            ((Ellipse)sender).Fill = new SolidColorBrush(Colors.CadetBlue);
+        }
+        private void elps_wizards_MouseLeave(object sender, RoutedEventArgs e)
+        {
+            int n = ((Ellipse)sender).Name.ToCharArray()[((Ellipse)sender).Name.Length - 1] - '0';
+            if(n-1 <= status)
+                ((Ellipse)sender).Fill = new SolidColorBrush(Colors.WhiteSmoke);
+            else
+                ((Ellipse)sender).Fill = ((Label)this.FindName("lbl_wizard_footer")).Background;
+        }
+        private void btn_next_Click(object sender, RoutedEventArgs e)
+        {
+            handleNewStatus();
+        }
+
+
+        private void elps_mouse_MouseDown(object sender, RoutedEventArgs e)
+        {
+            int n = ((Ellipse)sender).Name.ToCharArray()[((Ellipse)sender).Name.Length - 1] - '0';
+            if (n - 1 <= status)
+            {
+                status = n - 2;
+                handleNewStatus();
+            }
+        }
+        
+
+
+        private void handleNewStatus()
+        {
+            switch (status)
+            {
+                case -1:
+                    username = null;
+                    password = null;
+                    path = null;
+                    ((Label)this.FindName("lbl_wizard")).Content = "Inserisci il tuo username";
+                    ((TextBox)this.FindName("txt_wizard")).Text = "Username";
+                    ((Ellipse)this.FindName("circle1")).Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                    ((Ellipse)this.FindName("circle2")).Fill = ((Label)this.FindName("lbl_wizard_footer")).Background;
+                    ((Ellipse)this.FindName("circle3")).Fill = ((Label)this.FindName("lbl_wizard_footer")).Background;
+                    ((Ellipse)this.FindName("circle4")).Fill = ((Label)this.FindName("lbl_wizard_footer")).Background;
+                    status = 0;
+                    break;
+                case 0:
+                    username = ((TextBox)this.FindName("txt_wizard")).Text;
+                    password = null;
+                    path = null;
+                    ((Label)this.FindName("lbl_wizard")).Content = "Inserisci la password";
+                    ((TextBox)this.FindName("txt_wizard")).Text = "Password";
+                    ((Ellipse)this.FindName("circle1")).Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                    ((Ellipse)this.FindName("circle2")).Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                    ((Ellipse)this.FindName("circle3")).Fill = ((Label)this.FindName("lbl_wizard_footer")).Background;
+                    ((Ellipse)this.FindName("circle4")).Fill = ((Label)this.FindName("lbl_wizard_footer")).Background;
+                    status = 1;
+                    break;
+                case 1:
+                    username = ((TextBox)this.FindName("txt_wizard")).Text;
+                    password = null;
+                    path = null;
+                    ((Label)this.FindName("lbl_wizard")).Content = "Reinserisci la password";
+                    ((TextBox)this.FindName("txt_wizard")).Text = "Password";
+                    ((Ellipse)this.FindName("circle1")).Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                    ((Ellipse)this.FindName("circle2")).Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                    ((Ellipse)this.FindName("circle3")).Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                    ((Ellipse)this.FindName("circle4")).Fill = ((Label)this.FindName("lbl_wizard_footer")).Background;
+                    status = 2;
+                    break;
+
+
+            }
         }
     }
 }
